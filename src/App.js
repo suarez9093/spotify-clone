3d7e2a8ae8d5d2688af248de929411c9d45aa75f
suarez9093 <suarez9093@hotmail.com>
2021-03-12 16:36:13 -0500

    added login component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import Login from './components/Login';
 
 function App() {
-  return <div className='app'>app</div>;
+  return (
+    <div className='app'>
+      app
+      <Login />
+      {/* Spotify Logo */}
+      {/*  Login with spotify button */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Login.css';
+function Login() {
+  return (
+    <div className='login'>
+      <img
+        src='https://logos-world.net/wp-content/uploads/2020/09/Spotify-Logo.png'
+        alt='spotify logo'
+      />
+      <a>LOGIN WITH SPOTIFY</a>
+    </div>
+  );
+}
+
+export default Login;
