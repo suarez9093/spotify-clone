d5901ca9e8c606754cce278c450031ed309e7194
suarez9093 <suarez9093@hotmail.com>
2021-03-18 12:04:17 -0400

    structuring out play controls

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
-  return <div className='footer'>Footer</div>;
+  return (
+    <div className='footer'>
+      <div className='footer__left'>
+        <p>Albumn and song details</p>
+      </div>
+      <div className='footer__center'>
+        <p>player controls</p>
+      </div>
+      <div className='footer__right'>volume controls</div>
+    </div>
+  );
 }
 
 export default Footer;
