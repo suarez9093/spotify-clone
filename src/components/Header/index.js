13071dd8914d808e98eaa0b4fe4b12900282e4f3
suarez9093 <suarez9093@hotmail.com>
2021-03-18 12:55:05 -0400

    completed the Header. Search and user info done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,11 @@
+import { Avatar } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-import React from 'react';
+import React, { useContext } from 'react';
+import { context } from '../../context/index';
 import './Header.css';
 function Header() {
+  const { user } = useContext(context);
+  console.log('user', user);
   return (
     <header className='header'>
       <div className='header__left'>
@@ -11,7 +15,13 @@ function Header() {
           placeholder='Search for Artists, Songs or Playlists'
         />
       </div>
-      <div className='header__right'></div>
+      <div className='header__right'>
+        <Avatar
+          src={user ? user.images[0].url : null}
+          alt={user.display_name}
+        />
+        <h4>{user ? user.display_name : 'User'}</h4>
+      </div>
     </header>
   );
 }
