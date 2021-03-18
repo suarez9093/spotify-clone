1bf14b8b7fd4e185c711b9a218a70adce54d1b66
suarez9093 <suarez9093@hotmail.com>
2021-03-18 13:02:52 -0400

    created body info componentes

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -5,6 +5,17 @@ function Body({ spotify }) {
   return (
     <div className='body'>
       <Header />
+      <div className='body__info'>
+        <img
+          src='https://cdn.shortpixel.ai/client/q_lossy,ret_img,w_250,h_250/https://www.hypebot.com/wp-content/uploads/2020/07/discover-weekly-250x250.png'
+          alt=''
+        />
+        <div className='body__infoText'>
+          <strong>PLAYLISTS</strong>
+          <h2>Discover Weekly</h2>
+          <p>description</p>
+        </div>
+      </div>
     </div>
   );
 }
